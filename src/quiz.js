8111762e3cca544ee19ff6df78829f893c5084e7
Anuj Jhunjhunwala <anuj.jhunjhunwala@gmail.com>
2013-08-26 10:02:43 -0400

    set up object, can add numbers and arrays and can count number of elements. trying to flatten the array when an array is added

--- src/quiz.js
+++ src/quiz.js
@@ -6,3 +6,31 @@
 //      |__|     |__|  |__| |__| |_______/       |__| |_______/       /__/     \__\     \_____\_____\\______/  |__|  /________(__)
 
 var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
+
+
+function Quiz(){
+  this.numbers = [],
+  this.trash = [],
+  this.add = function(input){
+    if (typeof input === 'number' || input instanceof Array) {
+      this.numbers.push(input);
+      this.flatten(this.numbers);
+      return this.numbers
+    } else {
+      this.trash.push(input);
+    }
+  },
+  this.count = function(){
+    console.log(this.numbers.length);
+  },
+  this.flatten = function(array) {
+    var result = [], self = arguments.callee;
+    array.forEach(function(item) {
+      Array.prototype.push.apply(
+        result,
+        Array.isArray(item) ? self(item) : [item]
+        );
+    });
+    return result;
+  };
+}
