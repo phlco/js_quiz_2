a3d5df91bc420018e23de49d9e0fd0eea4a8029c
Theodore Cleveland <tcclevela@gmail.com>
2013-08-26 10:52:10 -0400

    almost finished with testing

--- spec/quizSpec.js
+++ spec/quizSpec.js
@@ -6,7 +6,6 @@
 // /__/     \__\ \______| /__/     \__\ |__| |__| \__|     __    (__)
 //                                                        (__)
 
-// See quiz.js for more details
 
 describe('a quiz', function() {
 
@@ -17,46 +16,94 @@ describe('a quiz', function() {
 
 });
 
-// Create a new quiz object
-// var quiz = new Quiz()
+describe('add function', function() {
 
-// We can add numbers to it
-// quiz.add(5)
-// quiz.add(3)
+	var quiz;
 
-// We can see the numbers we've added
-// quiz.numbers() => [5, 3]
+	beforeEach(function(){
+		quiz = new Quiz();
+	});
+	// We can add numbers to it
+	it('should be able to add numbers to the quiz"s numbers array', function() {
+		quiz.add(5);
+		quiz.add(3);
+		// We can see the numbers we've added
+		expect(quiz.numbers()).toEqual([5,3]);
+	});
 
-// We can only add numbers
-// quiz.add("7")
-// quiz.numbers() => [5, 3]
+	// We can only add numbers
+	it('should not add non-numbers', function() {
+		quiz.add(5);
+		quiz.add(3);
+		quiz.add("7");
+		expect(quiz.numbers()).toEqual([5,3]);
+	});
 
-// Or arrays of numbers
-// quiz.add([4, 2, true, 9])
-// quiz.numbers() => [5, 3, 4, 2, 9]
+	// Or arrays of numbers
+	it('should be able to add arrays of numbers', function() {
+		quiz.add([7,6,true,10]);
+		expect(quiz.numbers()).toEqual([7,6,10]);
+	});
 
-// We can't access the array directly
-// quiz = new Quiz();
-// quiz.add([1, 2, 3])
-// quiz.numbers().push(5)
-// quiz.numbers() => [1, 2, 3]
+	// We can't access the array directly
+	it('should not return it"s values', function() {
+		expect(quiz.values).toBeUndefined();
+	});
 
-// Trash returns anything we've tried to add that's not a number.
-// quiz.add(["7", true])
-// quiz.trash() => ["7", true]
+	it('should not allow to push into the returned numbers array', function() {
+		quiz.add([7,6,10]);
+		quiz.numbers().push(11);
+		expect(quiz.numbers()).toEqual([7,6,10]);
+	});
+});
 
-// Returns the number of occurences of a specified number
-// quiz.numbers() => [5, 3, 4, 3]
-// quiz.count(3) => 2
+describe('trash function', function(){
 
-// Takes a specified length of an array starting from the left and moves it to
-// the tail end
-// quiz = new Quiz()
-// quiz.add([1, 2, 3, 4, 5])
-// quiz.numbers() => [1, 2, 3, 4, 5]
-// quiz.rotate(2)
-// quiz.numbers() => [3, 4, 5, 1, 2]
+	// Trash returns anything we've tried to add that's not a number.
+	it('should return all non-number values passed to add function',function() {
+		var quiz = new Quiz();
+		quiz.add([10,true,'hello',false,42]);
+		expect(quiz.trash()).toEqual([true,'hello',false]);
+	});
 
-// Prints out the number of even numbers in the array.
-// quiz.numbers() => [1, 2, 3, 4, 5]
-// quiz.countEvent() => 2
+});
+
+
+describe('count', function() {
+	// Returns the number of occurences of a specified number
+	it('should return the number of times a specified number has been input', function() {
+		var quiz = new Quiz();
+		quiz.add([2,3,3,5,3,6,7,3,2,3]);
+		expect(quiz.count(3)).toEqual(5);
+	});
+
+	it('should return false if a a non-numbrer is input', function() {
+		var quiz = new Quiz();
+		quiz.add([2,3,3,5,3,6,7,3,2,3]);
+		expect(quiz.count('a')).toBe(false);
+	});
+});
+
+
+describe('rotate', function() {
+
+	// Takes a specified length of an array starting from the left and moves it to
+	// the tail end
+	it('should rotate the arry the specified number of times', function() {
+		var quiz = new Quiz();
+		quiz.add([2,3,4,5,6,7]);
+		expect(quiz.rotate(2)).toEqual([4,5,6,7,2,3]);
+	});
+
+	// Doesn't rotate negatively
+	it('should not rotate an array if a negative number is passed', function() {
+		var quiz = new Quiz();
+		quiz.add([2,3,4]);
+		expect(quiz.rotate(-3)).toEqual([2,3,4]);
+	});
+});
+
+
+describe('countEven', function() {
+
+});
--- src/quiz.js
+++ src/quiz.js
@@ -6,3 +6,63 @@
 //      |__|     |__|  |__| |__| |_______/       |__| |_______/       /__/     \__\     \_____\_____\\______/  |__|  /________(__)
 
 var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
+
+var Quiz = function() {
+
+	var values = [];
+	var bad_inputs = [];
+
+	return {
+
+		add: function(input) {
+			//input is a number
+			if(typeof(input)==='number') {
+				values.push(input);
+			// input is an array
+			} else if(input.length && typeof(input) != 'string') {
+				var i=0, max=input.length;
+				for(i; i < max; i+=1) {
+					this.add(input[i]);
+				}
+			// input is a non-number
+			} else {
+				bad_inputs.push(input);
+			}
+		},
+
+		numbers: function() {
+			var copy = [], i = 0, max = values.length;
+			for(i; i < max; i+=1) {
+				copy.push(values[i]);
+			}
+			return copy;
+		},
+
+		trash: function() {
+			var trash_copy = [], i = 0, max = bad_inputs.length;
+			for(i; i < max; i+=1) {
+				trash_copy.push(bad_inputs[i]);
+			}
+			return trash_copy;
+		},
+
+		count: function(match) {
+			if(typeof(match) === 'number'){
+				var i = 0, max = values.length, count = 0;
+				for(i; i < max; i+=1) {
+					if(values[i] === match) {count+=1;}
+				}
+				return count;
+			}
+			return false;
+		},
+
+		rotate: function(times) {
+			var i = 0; max = times; array = this.numbers();
+			for(i; i < max; i+=1) {
+				array.push(array.shift());
+			}
+			return array;
+		}
+	};
+};
