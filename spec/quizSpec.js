7da332324ea997360fc7e964d2e9d684e66db7ae
mvanger <mvanger@nyu.edu>
2013-08-26 10:00:52 -0400

    All but one

--- spec/quizSpec.js
+++ spec/quizSpec.js
@@ -15,25 +15,50 @@ describe('a quiz', function() {
     expect(QUIZ_TOPICS).toContain('javascript');
   });
 
-});
-
 // Create a new quiz object
 // var quiz = new Quiz()
+
+  it('makes a new quiz', function(){
+    var quiz = new Quiz();
+    expect(typeof(quiz)).toBe('object');
+  });
 
 // We can add numbers to it
 // quiz.add(5)
 // quiz.add(3)
-
 // We can see the numbers we've added
 // quiz.numbers() => [5, 3]
+
+  it('can have numbers added', function(){
+    var quiz = new Quiz();
+    quiz.add(5);
+    quiz.add(3);
+    expect(quiz.numbers).toEqual([5, 3]);
+  });
 
 // We can only add numbers
 // quiz.add("7")
 // quiz.numbers() => [5, 3]
 
+  it('can only add numbers', function(){
+    var quiz = new Quiz();
+    quiz.add(5);
+    quiz.add(3);
+    quiz.add("7");
+    expect(quiz.numbers).toEqual([5, 3]);
+  });
+
 // Or arrays of numbers
 // quiz.add([4, 2, true, 9])
 // quiz.numbers() => [5, 3, 4, 2, 9]
+
+  it('can also add arrays of numbers', function(){
+    var quiz = new Quiz();
+    quiz.add(5);
+    quiz.add(3);
+    quiz.add([4, 2, true, 9]);
+    expect(quiz.numbers).toEqual([5, 3, 4, 2, 9]);
+  });
 
 // We can't access the array directly
 // quiz = new Quiz();
@@ -41,13 +66,29 @@ describe('a quiz', function() {
 // quiz.numbers().push(5)
 // quiz.numbers() => [1, 2, 3]
 
+  it('cannot be accessed directly', function(){
+    expect("This doesn't work").toBe(false);
+  });
+
 // Trash returns anything we've tried to add that's not a number.
 // quiz.add(["7", true])
 // quiz.trash() => ["7", true]
 
+  it('returns trash', function(){
+    var quiz = new Quiz();
+    quiz.add(["7", true]);
+    expect(quiz.trash).toEqual(["7", true]);
+  });
+
 // Returns the number of occurences of a specified number
 // quiz.numbers() => [5, 3, 4, 3]
 // quiz.count(3) => 2
+
+  it('counts occurences of a specified number', function(){
+    var quiz = new Quiz();
+    quiz.add([5, 3, 4, 3]);
+    expect(quiz.count(3)).toEqual(2);
+  });
 
 // Takes a specified length of an array starting from the left and moves it to
 // the tail end
@@ -57,6 +98,30 @@ describe('a quiz', function() {
 // quiz.rotate(2)
 // quiz.numbers() => [3, 4, 5, 1, 2]
 
+  it('rotates the numbers array', function(){
+    var quiz = new Quiz();
+    quiz.add([1, 2, 3, 4, 5]);
+    quiz.rotate(2);
+    expect(quiz.numbers).toEqual([3, 4, 5, 1, 2]);
+  });
+
 // Prints out the number of even numbers in the array.
 // quiz.numbers() => [1, 2, 3, 4, 5]
-// quiz.countEvent() => 2
+// quiz.countEvens() => 2
+
+  it('prints out the number of even numbers in the array', function(){
+    var quiz = new Quiz();
+    quiz.add([1, 2, 3, 4, 5]);
+    expect(quiz.countEvens()).toEqual(2);
+  });
+
+});
+
+
+
+
+
+
+
+
+
--- src/quiz.js
+++ src/quiz.js
@@ -6,3 +6,48 @@
 //      |__|     |__|  |__| |__| |_______/       |__| |_______/       /__/     \__\     \_____\_____\\______/  |__|  /________(__)
 
 var QUIZ_TOPICS = ['javascript', 'jasmine', 'testing'];
+
+function Quiz(){
+  this.numbers = [];
+  this.trash = [];
+  this.add = function(number){
+    if (typeof(number) === 'number') {
+      this.numbers.push(number);
+    } else if (typeof(number) === 'object') {
+      for (i=0; i<number.length; i++) {
+        if (typeof(number[i]) === 'number') {
+          this.numbers.push(number[i]);
+        } else {
+          this.trash.push(number[i]);
+        }
+      }
+    } else {
+      this.trash.push(number);
+    }
+  };
+  this.count = function(number){
+    i = 0;
+    for (k=0; k<this.numbers.length; k++) {
+      if (this.numbers[k] === number) {
+        i++;
+      }
+    }
+    return i;
+  };
+  this.rotate = function(number){
+    for (i=0; i<number; i++) {
+      var num = this.numbers[0];
+      this.numbers.shift();
+      this.numbers.push(num);
+    }
+  };
+  this.countEvens = function(){
+    k = 0;
+    for (i=0; i<this.numbers.length; i++) {
+      if (this.numbers[i]%2 === 0) {
+        k++;
+      }
+    }
+    return k;
+  };
+}
